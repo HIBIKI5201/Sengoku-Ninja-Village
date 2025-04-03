a6dfcfdaede20d1329d7ad750910508640498a94
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-03 11:16:26 +0900

    [add]システムシーンのマネージャーを追加

---
+++ Assets/Scripts/Runtime/System/SystemManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace SengokuNinjaVillage.Runtime.System
+{
+    /// <summary>
+    /// システムシーンのマネジメントを行う
+    /// </summary>
+    public class SystemManager : MonoBehaviour
+    {
+
+    }
+}
