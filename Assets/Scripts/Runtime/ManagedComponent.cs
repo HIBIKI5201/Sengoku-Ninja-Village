dec9cc839a0dafabaa99ad81d1292c38ebc8a89f
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-03 11:32:47 +0900

    [update]マネジメントされたコンポーネント

---
+++ Assets/Scripts/Runtime/ManagedComponent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace SengokuNinjaVillage.Runtime.System
+{
+    /// <summary>
+    /// シーンのシステムにマネジメントされるコンポーネント
+    /// </summary>
+    public class ManagedComponent : MonoBehaviour
+    {
+        /// <summary>
+        /// シーンシステムによるAwake
+        /// </summary>
+        public virtual void ManagedAwake() { }
+
+        /// <summary>
+        /// シーンシステムによるUpdate
+        /// </summary>
+        public virtual void ManagedStart() { }
+
+        /// <summary>
+        /// シーンシステムによるUpdate
+        /// </summary>
+        public virtual void ManagedUpdate() { }
+    }
+}
