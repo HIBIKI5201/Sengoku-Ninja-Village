0644323f4135fa1e1561118b46601d8c9d6ac288
NaritaShosei <onigirikun7171@gmail.com>
2025-04-06 17:50:14 +0900

    [fix]フォルダの場所変更

---
+++ Assets/Scripts/Runtime/InGame/Player/PlayerManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SengokuNinjaVillage
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerManager : MonoBehaviour
+    {
+        [SerializeField] InputAction _input;
+        Rigidbody _rb;
+        Vector2 _inputVector;
+        float _moveSpeed = 5;
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+        void OnEnable()
+        {
+            _input.Enable();
+            _input.started += Input;
+            _input.performed += Input;
+            _input.canceled += Input;
+        }
+
+        void OnDisable()
+        {
+            _input.started -= Input;
+            _input.performed -= Input;
+            _input.canceled -= Input;
+            _input.Disable();
+        }
+        private void Update()
+        {
+            Move(_inputVector);
+        }
+
+        void Input(InputAction.CallbackContext context)
+        {
+            _inputVector = _input.ReadValue<Vector2>();
+        }
+
+        void Move(Vector2 input)
+        {
+            var forward = Camera.main.transform.forward;
+            var right = Camera.main.transform.right;
+
+            forward.y = 0;
+            right.y = 0;
+            forward.Normalize();
+            right.Normalize();
+
+            var moveVector = forward * input.y + right * input.x;
+            _rb.linearVelocity = moveVector * _moveSpeed;
+        }
+    }
+}
