e90492631004ac798a04173286abcfe89a5317e8
NaritaShosei <onigirikun7171@gmail.com>
2025-04-14 03:07:19 +0900

    [update]カメラの距離を動的に変更する

--- Assets/Scripts/Runtime/InGame/Player/CameraManager.cs
+++ Assets/Scripts/Runtime/InGame/Player/CameraManager.cs
@@ -1,4 +1,5 @@
 ﻿using SengokuNinjaVillage.Runtime.System;
+using Unity.Cinemachine;
 using UnityEngine;
 using static SengokuNinjaVillage.Runtime.System.InputManager;
 
@@ -7,13 +8,15 @@ namespace SengokuNinjaVillage.Runtime.InGame
     public class CameraManager : MonoBehaviour
     {
         Vector2 _inputRotateVector;
-        [SerializeField, Header("カメラの横回転速度"), Range(0.1f, 1)] float _rotationSpeedX;
-        [SerializeField, Header("カメラの縦回転速度"), Range(0.1f, 1)] float _rotationSpeedY;
+        [SerializeField, Header("カメラの横回転速度"), Range(0.1f, 1)] float _rotationSpeedX = 0.1f;
+        [SerializeField, Header("カメラの縦回転速度"), Range(0.1f, 1)] float _rotationSpeedY = 0.1f;
         [SerializeField, Header("カメラの縦回転できる限界点")] float _rotationClamp = 30;
+        [SerializeField, Header("カメラとプレイヤーの距離"), Range(1, 5)] float _cameraDistance = 1;
         [SerializeField] bool _invertX;
         [SerializeField] bool _invertY;
         [SerializeField] Transform _eye;
         [SerializeField] Transform _player;
+        [SerializeField] CinemachineThirdPersonFollow _vcam;
         private void OnEnable()
         {
             AddAction<Vector2>(InputKind.Look, InputTriggerType.Performed, OnRotateInput);
@@ -24,9 +27,13 @@ namespace SengokuNinjaVillage.Runtime.InGame
             RemoveAction<Vector2>(InputKind.Look, InputTriggerType.Performed, OnRotateInput);
             RemoveAction<Vector2>(InputKind.Look, InputTriggerType.Canceled, OnRotateInput);
         }
-
+        private void Start()
+        {
+            SetDistance(_cameraDistance);
+        }
         void Update()
         {
+            SetDistance(_cameraDistance);
             Rotation(_inputRotateVector);
         }
         void OnRotateInput(Vector2 input)
@@ -50,6 +57,9 @@ namespace SengokuNinjaVillage.Runtime.InGame
 
             _eye.localEulerAngles = new Vector3(newX, eyeEuler.y, eyeEuler.z);
         }
-
+        void SetDistance(float dis)
+        {
+            _vcam.CameraDistance = dis;
+        }
     }
 }
